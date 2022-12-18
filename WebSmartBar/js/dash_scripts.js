9edde374e13694dcd0366d2ef2307fb7626752de
devNorth1991 <gustavobarrera22@yahoo.com.ar>
2022-12-18 00:44:08 -0300

    validaciones de generar qr terminadas y probadas

--- WebSmartBar/js/dash_scripts.js
+++ WebSmartBar/js/dash_scripts.js
@@ -3,9 +3,16 @@
 
 const apiGetSectores = "http://localhost/SmartBarApi/Clases/GetResponse/ConsultarSectores.php";
 
-const apiGetMesas = "http://localhost/SmartBarApi/Clases/GetResponse/ConsultarObjetosGral.php";
+const apiGetMesas = "http://localhost/SmartBarApi/Clases/GetResponse/ConsultarMesas.php";
 
 
+const apiChangeEStadosMesas = "http://localhost/SmartBarApi/Clases/PutResponse/ActualizarEstadoObjeto.php"
+
+
+const apiGetMesasConPedidosListos = "http://localhost/SmartBarApi/Clases/GetResponse/ConsultarMesaByPedido.php";
+
+
+const apiGetEStadoYPedidosById = "http://localhost/SmartBarApi/Clases/GetResponse/ConusltarEstadoCuentaMasPedidosById.php?ObjId=";
 
 //dashboard tags
 const sideMenu = document.querySelector("#aside-dash");
@@ -13,6 +20,7 @@ const menuBtnDash = document.getElementById("menu-btn-dash");
 const closeBtnDash = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
 const containerTableQr = document.querySelector(".table-container");
+const containerTableOrders = document.querySelector(".vista-pedidos-container");
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector('.modal_close');
 const contenedorQr = document.querySelector('.qr_container');
@@ -28,10 +36,15 @@ const fragment = document.createDocumentFragment();
 
 
 //*******************************************variables globales*****************************************//
+const Estados = {
 
 
-let arrayMesasCombined = [];
+   "Libre": 1,
+   "Ocupado": 2,
+   "Reservado": 3,
+   "Inhabilitado": 4,
 
+}
 
 
 
@@ -180,24 +193,20 @@ const getMesas = async () => {
       if (res.status === 200) {
 
          const data = await res.json();
-         console.log(data);
-         let arrayMesasLibres = [];
+
          let arrayMesasRservadas = [];
          let arrayMesasOcupadas = [];
-
-         arrayMesasLibres = [...filterMesa(data, "Libre")];
-         console.log(arrayMesasLibres);
-
-         arrayMesasRservadas = [...filterMesa(data, "Reservado")];
-         console.log(arrayMesasRservadas);
+         let arrayMesasLibres = [];
 
          arrayMesasOcupadas = [...filterMesa(data, "Ocupado")];
-         console.log(arrayMesasOcupadas);
 
-         arrayMesasCombined = arrayMesasLibres.concat(arrayMesasRservadas).concat(arrayMesasOcupadas);
+         arrayMesasRservadas = [...filterMesa(data, "Reservado")];
 
-         // console.log(arrayMesasCombined);
-         pintarCardTables(arrayMesasCombined);
+         arrayMesasLibres = [...filterMesa(data, "Libre")];
+
+         return arrayMesasCombined = [...arrayMesasOcupadas, ...arrayMesasRservadas, ...arrayMesasLibres];
+
+
 
       } else if (res.status === 401) {
 
@@ -221,6 +230,96 @@ const getMesas = async () => {
 
 
 }
+
+const getTablesWithOrders = async () => {
+
+   try {
+
+      const res = await fetch(apiGetMesasConPedidosListos)
+      const data = await res.json()
+      pintarCardTablesOrders(data);
+
+   } catch (error) {
+
+      console.log('ha ocurrido el sgte error: ' + error)
+
+   }
+
+
+}
+
+
+const pintarCardTablesOrders = (data) => {
+
+   data.forEach(mesa => {
+
+      templateCardTable.getElementById("qrTableContainer").classList.add("ocupada");
+      templateCardTable.getElementById("qrTableContainer").classList.add("pedidoListo");
+      templateCardTable.querySelector(".templateCardTables-accion").style.visibility = "hidden";
+      templateCardTable.querySelector('.title-card').textContent = mesa.Obj_Codigo;
+      templateCardTable.querySelector('.footer-card').textContent = `CAP.${mesa.Obj_Capacidad}`;
+      templateCardTable.querySelector('.idMesa').textContent = mesa.Obj_Id;
+
+      const clone = templateCardTable.cloneNode(true);
+      fragment.appendChild(clone);
+
+   })
+
+   containerTableOrders.appendChild(fragment);
+
+}
+
+
+
+// const getMesasConPedidosListos = async () => {
+
+//    try {
+
+
+//       fetch(apiGetMesasConPedidosListos)
+//       .then(response => response.json())
+//       .then(data => {
+//       // data es un array de objetos JSON
+//        const arrayOfObjects = JSON.parse(data);
+//       // arrayOfObjects es ahora un array de objetos JavaScript
+//       console.log(arrayOfObjects);
+//     });
+
+//       // const res = await fetch(apiGetMesasConPedidosListos);
+//       // if (res.status === 200) {
+
+//       //    const data = await res.json();
+
+//       //    const arrayMesasConPedidosListos = JSON.parse(data);
+
+//       //    console.log("array de mesas con pedidos Listos");
+//       //    console.log(arrayMesasConPedidosListos);
+//       //   return arrayMesasConPedidosListos;
+
+
+//       // } else if (res.status === 401) {
+
+//       //    console.log('pusiste tu api key de manera incorrecta');
+
+//       // } else if (res.status === 404) {
+
+//       //    console.log('El menu que buscas no existe');
+
+//       // } else {
+
+//       //    console.log('Hubo Un error Y no sabemos que paso');
+
+//       // }
+
+//    } catch (error) {
+
+//       console.log(error);
+
+//    }
+
+
+// }
+
 
 
 //filtrado 
@@ -256,7 +355,9 @@ const filterMesa = (array, key) => {
 
 //pintar mesas dentro de generador de qr
 
-const pintarCardTables = (data) => {
+async function pintarCardTables() {
+
+   const data = await getMesas();
 
 
    data.forEach(mesa => {
@@ -266,29 +367,66 @@ const pintarCardTables = (data) => {
       templateCardTable.querySelector('.idMesa').textContent = mesa.Obj_Id;
 
 
-      if (mesa.ObjEs_Descripcion == "Reservado") {
+      console.log(`el estado de la mesa ${mesa.Obj_Codigo} es igual a => ${mesa.ObjEs_Descripcion}`);
 
-         templateCardTable.querySelector("div.libre").classList.add("reservada");
+      let EstadoDeMesa = mesa.ObjEs_Descripcion;
 
+      switch (EstadoDeMesa) {
 
-      } else if (mesa.ObjEs_Descripcion == "Ocupado") {
+         case 'Libre':
 
-         templateCardTable.querySelector("div.libre").classList.add("ocupada");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("reservada");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("ocupada");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("cancelada");
+            templateCardTable.getElementById("qrTableContainer").classList.add("libre");
+            templateCardTable.querySelector(".templateCardTables-accion").style.visibility = "visible";
+            templateCardTable.querySelector('.turnStatusCheck').dataset.id = mesa.Obj_Id;
+            console.log(`el id de la mesa ${mesa.Obj_Codigo} es ${mesa.Obj_Id}`)
+            break;
+
+         case 'Reservado':
+
+            templateCardTable.getElementById("qrTableContainer").classList.remove("libre");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("ocupada");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("cancelada");
+            templateCardTable.getElementById("qrTableContainer").classList.add("reservada");
+            templateCardTable.querySelector(".templateCardTables-accion").style.visibility = "visible";
+            templateCardTable.querySelector('.turnStatusCheck').dataset.id = mesa.Obj_Id;
+            break;
+
+         case 'Ocupado':
+
+            templateCardTable.getElementById("qrTableContainer").classList.remove("libre");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("reservada");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("cancelada");
+            templateCardTable.getElementById("qrTableContainer").classList.add("ocupada");
+            templateCardTable.querySelector(".templateCardTables-accion").style.visibility = "hidden";
+            break;
+
+         case 'Inhabilitado':
+
+            templateCardTable.getElementById("qrTableContainer").classList.remove("libre");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("reservada");
+            templateCardTable.getElementById("qrTableContainer").classList.remove("ocupada");
+            templateCardTable.getElementById("qrTableContainer").classList.add("cancelada");
+            templateCardTable.querySelector(".templateCardTables-accion").style.visibility = "hidden";
+            break;
+
+         default: alert('falla el renderizado')
+            break;
 
       }
+
       const clone = templateCardTable.cloneNode(true);
       fragment.appendChild(clone);
 
    })
-   containerTableQr.appendChild(fragment);
 
+   containerTableQr.appendChild(fragment);
 
 }
 
-
-
-
-/***********************Agregamos Click al cpntenedor de mesas *************************** */
+/***********************Agregamos Click al contenedor de mesas *************************** */
 
 containerTableQr.addEventListener('click', (e) => {
 
@@ -297,10 +435,70 @@ containerTableQr.addEventListener('click', (e) => {
 
       console.log(e.target.classList);//de esta manera con solo un addEven Listener detectamos cualquier Click
       generarEvent(e);
+
+
+      /*********************************************evento para cambiar de estado a las mesas *********************/
+
+   } else if (e.target.classList.contains('turnStatusCheck')) {
+
+      generateEventTwo(e);
+
    }
    e.stopPropagation();
 })
 
+
+
+
+const generateEventTwo = e => {
+
+   if (e.target.classList.contains('turnStatusCheck')) {
+
+      changeStatus(e.target.parentElement)//enviamos el contenido de la card al metodo generar qr 
+
+   }
+
+
+}
+
+const changeStatus = objeto => {
+
+   let idObj = objeto.querySelector('.turnStatusCheck').dataset.id;
+   let idEst = Estados.Ocupado;
+
+   const params = {
+      idObj,
+      idEst
+   };
+
+   objeto.querySelector('.turnStatusCheck').disabled = true;
+
+   fetch(apiChangeEStadosMesas, {
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers: { "Content-type": "application/json; charset=UTF-8" }
+   })
+      .then(response => response.json())
+      .then(data => {
+         const esExito = data.exito;
+         console.log(esExito); // imprime "true"
+
+         if (esExito) {
+            alert("se ha cambiado el estado de la mesa Con exito")
+            while (containerTableQr.firstChild) {
+               containerTableQr.removeChild(containerTableQr.firstChild);
+            }
+
+            pintarCardTables();
+
+
+         } else {
+
+            alert("No se pudo actualizar el objeto")
+         }
+      })
+      .catch(err => console.log(err));
+}
 
 const generarEvent = e => {
 
@@ -313,131 +511,92 @@ const generarEvent = e => {
 
 
 
+
 var qrcode = new QRCode(contenedorQr)
 
 const generarQr = objeto => {
 
-   console.log('este es el objeto que recibe el modal generar qr')
-   console.log(objeto)
+
 
    let ObjId = objeto.querySelector('.idMesa').textContent;
 
+   let CuentaAbierta = false;
+   let pedidosSinEntregar = false;
+
    if (objeto.classList.contains("ocupada")) {
 
-      console.log('es una mesa ocupada')
 
-      const urlGetCuenById="http://localhost/SmartBarApi/Clases/GetResponse/ConsultarMesaStatusCuentaByIdMesa.php?ObjId=1";
-
-      /*aqui hariamos la consulta fetch de si ya tiene una cuenta abierta y si tienen pedidos abiertos*/
-
-      /*
-      
-      const url = 'https://mi-sitio-web.com/api/recursos'; // URL de la API
-
-const params = {
-  param1: 'valor1', // valores de los parámetros
-  param2: 'valor2'
-};
-
-fetch(url, {
-  method: 'GET', // método de la solicitud
-  headers: {
-    // cabeceras de la solicitud
-    'Content-Type': 'application/json'
-  },
-  params: new URLSearchParams(params) // parámetros de la solicitud
-})
-  .then(response => response.json()) // procesar la respuesta como JSON
-  .then(data => {
-    // aquí puedes hacer algo con los datos de la respuesta
-  })
-  .catch(error => {
-    // manejar cualquier error
-  });
-      
-      
-      
-      
-      */
+      const getMesaById = async () => {
 
 
-  //OTRA SOLUCION 
+         fetch(apiGetEStadoYPedidosById + ObjId)
+            .then(response => response.json())
+            .then(data => {
+               const [mesaConsultada] = data;
+
+               isOk = Validar(mesaConsultada);
 
 
-  /*
-  
-  const url = 'https://mi-sitio-web.com/api/recursos'; // URL de la API
+            });
 
-const params = {
-  param1: 'valor1' // valor del parámetro
-};
+      }
 
-fetch(url, {
-  method: 'GET', // método de la solicitud
-  headers: {
-    // cabeceras de la solicitud
-    'Content-Type': 'application/json'
-  },
-  params: new URLSearchParams(params) // parámetros de la solicitud
-})
-  .then(response => response.json()) // procesar la respuesta como JSON
-  .then(data => {
-    // aquí puedes hacer algo con los datos de la respuesta
-  })
-  .catch(error => {
-    // manejar cualquier error
-  });
-En este ejemplo, estamos haciendo una solicitud GET a la URL de una API, y pasando un parámetro como parte de la solicitud. El parámetro se convierte en un objeto URLSearchParams y se pasa como una opción de params al método fetch().
+      getMesaById();
 
-Cuando se recibe la respuesta de la solicitud, se procesa como un objeto JSON y luego se puede hacer algo con los datos recibidos. Si se produce algún error durante la solicitud, se maneja en el bloque catch().
 
-Espero que esto te ayude. Si tienes alguna pregunta adicional, no dudes en hacerla. ¡Buena suerte!
+      function Validar(mesa) {
+
+         console.log(mesa);
+         console.log(`el objeto tiene cuenta: ${mesa.CuenEs_Descripcion}`);
+         console.log(`y el estado del ultimo pedido es : ${mesa.pedEs_Descripcion}`);
+
+         if (mesa.CuenEs_Descripcion == "Abierta") {
+
+
+            alert("la cuenta Esta abierta");
+
+            return false;
+
+         } else if (mesa.pedEs_Descripcion != "Entregado" && mesa.pedEs_Descripcion != "Cancelado") {
+
+            alert("la mesa posee pedidos sin entregar");
+            return false;
+
+
+         } else if (mesa.CuenEs_Descripcion == "Cerrada" && mesa.pedEs_Descripcion == "Entregado") {
+
+            modal.classList.add("modal--show");
+
+            let url = "https://wa.me/+542474512011?text=frontsmartbar.netlify.app/?CuenCod="
+            let CuenCod = objeto.querySelector('.title-card').textContent;
+            let ampersand = "%26ObjId=";
+            let fechaActual = new Date().toISOString(); //2022-11-20T03:42:45.831Z
+            let fechaTratada = removeCharacterFromString(fechaActual);
+
+            let urlCompleta = url.concat(CuenCod).concat('-').concat(ObjId).concat('-').concat(fechaTratada).concat(ampersand).concat(ObjId)
+            console.log(`la urlCompleta es : ${urlCompleta}`)
+
+            qrcode.makeCode(urlCompleta);
+            modalClose.addEventListener('click', (e) => {
+               qrcode.clear();
+               e.preventDefault();
+               modal.classList.remove('modal--show');
+            })
+
+            return true;
+
+         }
 
 
 
+      }
 
+   } else {
 
-  
-  
-  
-  */
-
-
+      alert("Debes Pasar la mesa a Ocupada para poder generar Un qr")
 
 
    }
-
-
-
-   /*Logica antes de mostrar qr si la mesa esta ocupada tiene cuenCod abierta  y tiene pedidos listos registrados o en elaboracion
-   
-   mostrar modal alternativo No se puede generar qr hasta que no se cierre la cuenta y se cancelen o entreguen los pedidos
-
-
-   */
-
-   // si esta todo bien hace lo siguiente 
-
-
-   modal.classList.add("modal--show");
-
-   let url = "https://wa.me/+542474512011?text=frontsmartbar.netlify.app/?CuenCod="
-   let CuenCod = objeto.querySelector('.title-card').textContent;
-   let ampersand = "%26ObjId=";
-   let fechaActual = new Date().toISOString(); //2022-11-20T03:42:45.831Z
-   let fechaTratada = removeCharacterFromString(fechaActual);
-
-   let urlCompleta = url.concat(CuenCod).concat('-').concat(ObjId).concat('-').concat(fechaTratada).concat(ampersand).concat(ObjId)
-   console.log(`la urlCompleta es : ${urlCompleta}`)
-
-   qrcode.makeCode(urlCompleta);
-   modalClose.addEventListener('click', (e) => {
-      qrcode.clear();
-      e.preventDefault();
-      modal.classList.remove('modal--show');
-   })
-
-
 }
 
 
@@ -463,13 +622,8 @@ const removeCharacterFromString2 = (dataString) => {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
-   // getSectores();
-   getMesas();
-
-
-
-
+   getTablesWithOrders();
+   pintarCardTables();
 
 
 })
